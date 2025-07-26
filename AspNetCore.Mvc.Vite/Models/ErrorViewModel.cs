a9f6b1a9825eccc851ad925f1f8230abdf805357
Marco <marco_antonio_099@hotmail.com>
2025-07-26 16:28:39 +0100

    Setup empty .NET Core 8.0 MVC project

---
+++ AspNetCore.Mvc.Vite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetCore.Mvc.Vite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
