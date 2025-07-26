add5e8b3f884c6e8763ada3b2117874e716afd08
Marco <marco_antonio_099@hotmail.com>
2025-07-26 17:29:52 +0100

    vite.config added post action to fix file path in the manifest.json

--- AspNetCore.Mvc.Vite/Vite/vite.config.js
+++ AspNetCore.Mvc.Vite/Vite/vite.config.js
@@ -3,43 +3,48 @@ import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import fs from 'fs';
 
-const defaultDirName = path.resolve(__dirname, '../wwwroot');
-
-function fixPartialFilePath(filePath) {
-    return filePath.replace("//", "/").replace("\\", "/").replace("\\\\", "/");
-}
-
-function getAllFiles(dir, fileList = []) {
-    // If directory doesn't exist, return empty array
-    if (!fs.existsSync(dir)) {
-        return [];
-    }
-
-    // Read all files and directories in the given directory
-    fs.readdirSync(dir).forEach(file => {
-        const fullPath = path.resolve(dir, file);
-
-        if (fs.statSync(fullPath).isDirectory()) {
-            getAllFiles(fullPath, fileList);
-        } else {
-            if (/\.(js|ts|jsx|tsx|css)$/.test(fullPath)) {
-                fileList.push(fullPath);
-            }
-        }
-    });
-
-    return fileList;
-}
+const partialDirName = '../wwwroot';
+const partialDirPath = path.resolve(__dirname, partialDirName);
 
 function getInputFiles(rootFolders = []) {
+    function normalizeUrl(url) {
+        return url
+            .replace(/\\+/g, '/')
+            .replace(/\/+/g, '/')
+            .replace(/^\/|\/$/g, '')
+            .replace(partialDirName, '');
+    }
+
+    function getAllFiles(dir, fileList = []) {
+        // If directory doesn't exist, return empty array
+        if (!fs.existsSync(dir)) {
+            return [];
+        }
+
+        // Read all files and directories in the given directory
+        fs.readdirSync(dir).forEach(file => {
+            const fullPath = path.resolve(dir, file);
+
+            if (fs.statSync(fullPath).isDirectory()) {
+                getAllFiles(fullPath, fileList);
+            } else {
+                if (/\.(js|ts|jsx|tsx|css)$/.test(fullPath)) {
+                    fileList.push(fullPath);
+                }
+            }
+        });
+
+        return fileList;
+    }
+
     const inputFiles = {};
 
     if (rootFolders.length > 0) {
         for (const rootFolder of rootFolders) {
-            const rootFolderDir = path.resolve(defaultDirName, rootFolder);
+            const rootFolderDir = path.resolve(partialDirPath, rootFolder);
 
             getAllFiles(rootFolderDir).forEach(file => {
-                inputFiles[fixPartialFilePath(path.relative(defaultDirName, file))] = fixPartialFilePath(file);
+                inputFiles[normalizeUrl(path.relative(partialDirPath, file))] = normalizeUrl(file);
             });
         }
     }
@@ -47,9 +52,35 @@ function getInputFiles(rootFolders = []) {
     return inputFiles;
 }
 
+function modifyManifestKeys() {
+    return {
+        name: 'modify-manifest-keys',
+        apply: 'build',
+        closeBundle() {
+            const manifestPath = path.resolve(partialDirPath, 'dist/.vite/manifest.json');
+
+            if (fs.existsSync(manifestPath)) {
+                const manifest = JSON.parse(fs.readFileSync(manifestPath, 'utf-8'));
+
+                const updatedManifest = {};
+                for (const key in manifest) {
+
+                    // Remove "../wwwroot" from the key
+                    if (manifest.hasOwnProperty(key)) {
+                        const updatedKey = key.replace(/^(\.\.\/wwwroot)/, '');
+                        updatedManifest[updatedKey] = manifest[key];
+                    }
+                }
+
+                // Write the modified manifest back to the file
+                fs.writeFileSync(manifestPath, JSON.stringify(updatedManifest, null, 2));
+            }
+        },
+    };
+}
+
 export default defineConfig(({ command }) => ({
     base: command === 'build' ? '/dist/' : '/',
-    plugins: [vue()],
     build: {
         outDir: '../wwwroot/dist',
         manifest: true,
@@ -58,4 +89,5 @@ export default defineConfig(({ command }) => ({
             input: getInputFiles(["js", "css"])
         }
     },
+    plugins: [vue(), modifyManifestKeys()],
 }));
